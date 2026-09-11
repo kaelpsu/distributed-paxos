161ce49812e85f6aaa1bc2be414dfc3d5cb12c02
Kael Paraguassu <kaelparaguassu@hotmail.com>
2026-09-11 15:51:40 -0300

    refactor: change deserialization parameter type to better work with tcp

--- src/main/java/ufrn/kael/distributedPaxos/common/serialization/JsonSerializer.java
+++ src/main/java/ufrn/kael/distributedPaxos/common/serialization/JsonSerializer.java
@@ -1,5 +1,8 @@
 package ufrn.kael.distributedPaxos.common.serialization;
 
+import java.io.InputStream;
+
+import tools.jackson.core.StreamReadFeature;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.json.JsonMapper;
 import ufrn.kael.distributedPaxos.common.message.Message;
@@ -9,14 +12,17 @@ public class JsonSerializer implements Serializer {
     private final ObjectMapper mapper;
 
     public JsonSerializer() {
-        this.mapper = JsonMapper.builder().findAndAddModules().build();
+        this.mapper = JsonMapper.builder()
+                        .findAndAddModules()
+                        .disable(StreamReadFeature.AUTO_CLOSE_SOURCE) // prevents jackson from closing connection right after deserialization (we need it for responses)
+                        .build();
     }
 
     public byte[] serialize(Message message) {
         return mapper.writeValueAsBytes(message);
     }
 
-    public Message deserialize(byte[] data) {
-        return mapper.readValue(data, Message.class);
+    public Message deserialize(InputStream inputStream) {
+        return mapper.readValue(inputStream, Message.class);
     }
 }
--- src/main/java/ufrn/kael/distributedPaxos/common/serialization/Serializer.java
+++ src/main/java/ufrn/kael/distributedPaxos/common/serialization/Serializer.java
@@ -1,10 +1,12 @@
 package ufrn.kael.distributedPaxos.common.serialization;
 
+import java.io.InputStream;
+
 import ufrn.kael.distributedPaxos.common.message.Message;
 
 public interface Serializer {
     byte[] serialize(Message message);
 
-    Message deserialize(byte[] data);
+    Message deserialize(InputStream inputStream);
     
 }

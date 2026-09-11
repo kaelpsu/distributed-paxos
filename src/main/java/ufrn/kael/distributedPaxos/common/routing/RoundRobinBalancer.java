840a657e098860d203d9c0d7892ed3f0a22687ca
Kael Paraguassu <kaelparaguassu@hotmail.com>
2026-09-11 17:23:33 -0300

    feat: add simple round robin balancer

---
+++ src/main/java/ufrn/kael/distributedPaxos/common/routing/RoundRobinBalancer.java
@@ -0,0 +1,18 @@
+package ufrn.kael.distributedPaxos.common.routing;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RoundRobinBalancer {
+    
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    public String getNextNode(List<String> activeNodes) {
+        if (activeNodes == null || activeNodes.isEmpty()) {
+            throw new IllegalStateException("Service Unavailable: There are no active Business Nodes.");
+        }
+        
+        int index = counter.getAndIncrement() % activeNodes.size();
+        return activeNodes.get(index);
+    }
+}
